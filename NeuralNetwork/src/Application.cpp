161bc027fae17daa1e6754f73e4b270633e61321
PuckInside <saginov606@gmail.com>
2025-04-03 05:47:56 +0500

    Test Perceptron

--- NeuralNetwork/src/Application.cpp
+++ NeuralNetwork/src/Application.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-#include "Layers/Linear.h"
+#include <vector>
+#include <memory>
+
+#include "Models/Perceptron.h"
 #include "Loss/MSELoss.h"
-#include "Layers/ActivationLayers/Sigmoid.h"
 
 void main()
 {
@@ -13,33 +15,18 @@ void main()
     Eigen::MatrixXd target(3, 1);
     target << 0, 1.0, 0.0;
 
+    Perceptron model(4, 120, 3, 2);
     MSELoss loss;
-    Sigmoid sigmoid;
-    Linear linearRegression(4, 3);
 
-    Eigen::MatrixXd predict; 
-    predict = linearRegression.Forward(inputs);
-    predict = sigmoid.Forward(predict);
+    Eigen::MatrixXd predict;
+    predict = model.Prediction(inputs);
 
     std::cout << "Initial prediction:\n" << predict << "\n\n";
     std::cout << "Initial loss:\n" << loss.Loss(predict, target) << "\n\n";
 
-    for (int i = 0; i < 50; i++)
-    {
-        predict = linearRegression.Forward(inputs);
-        predict = sigmoid.Forward(predict);
-        predict = sigmoid.Forward(predict);
-
-        Eigen::MatrixXd grad;
-        grad = loss.LossPrime(predict, target);
-        grad = sigmoid.Backward(grad, 0.001);
-        linearRegression.Backward(grad, 0.001);
-    }
-
-    predict = linearRegression.Forward(inputs);
-    predict = sigmoid.Forward(predict);
+    model.Fit(inputs, target, 0.01, 10000);
+    predict = model.Prediction(inputs);
 
     std::cout << "Final prediction:\n" << predict << "\n\n";
     std::cout << "Final loss:\n" << loss.Loss(predict, target) << "\n\n";
-
 }
